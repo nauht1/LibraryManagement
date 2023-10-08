7be6f03e56bde87bb38d2a34ac59542c5f67acae
KhaiHoang <hoangkhai178@gmail.com>
2023-10-08 14:37:51 +0700

    save

---
+++ BALayer/DBTheThuVien.cs
@@ -0,0 +1,25 @@
+﻿using DBLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BALayer
+{
+    public class DBTheThuVien
+    {
+        DAL db = null;
+        public DBTheThuVien()
+        {
+            db = new DAL();
+        }
+        public DataSet LayToanBoTheThuVien()
+        {
+            return db.ExecuteQueryDataSet("Select * from TheThuVien", CommandType.Text, null);
+        }
+
+    }
+}
